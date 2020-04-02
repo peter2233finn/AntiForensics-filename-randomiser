0745c5b2b33d1affa7ce6451032f2a3401e59b06
Peter Finn <c16407722@mydit.ie>
2020-04-02 14:06:23 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
+echo "Copying into /usr/bin and /usr/sbin..."
 sudo cp filernd /usr/bin
 sudo cp filernd /usr/sbin
+echo "Making executible...."
 sudo chmod +x /usr/bin/filernd
 sudo chmod +x /usr/bin/filernd
+echo "All done. Run filernd to use"
